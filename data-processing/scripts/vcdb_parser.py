49ad8877a1d916d399e7aa2ea25a3c13fab9507b
unknown <georgejosephhamilton@gmail.com>
2022-06-07 17:22:26 -0400

    Update Data Parsing
    Update data parsing script to remove threat actors which will not be used in the model. Introduce formatting changes to increase pylint compliance.

--- data-processing/scripts/vcdb_parser.py
+++ data-processing/scripts/vcdb_parser.py
@@ -8,27 +8,29 @@ import json
 class Incident:
     # Track breach vs. non-breach incidents for each actor in all incidents
     all_actor_fail = {}  # actor: {'success', 'fail', 'maybe', 'fail-rate'}
-
+    all_actor_count = 0
     # Track all incidents for each actor if the victim is a small business
     sb_actor_prevalence = {}  # actor: {'count', 'prevalence-rate'}
     sb_incident_count = 0
 
-    # target_employee_counts = ['1 to 10', '11 to 100', '101 to 1000', '1001 to 10000', '10001 to 25000',
-    #                           '25001 to 50000', '50001 to 100000', 'Over 100000', 'Small', 'Large', 'Unknown']
+    # target_employee_counts = ['1 to 10', '11 to 100', '101 to 1000', '1001 to 10000',
+    #                           '10001 to 25000', '25001 to 50000', '50001 to 100000',
+    #                           'Over 100000', 'Small', 'Large', 'Unknown']
     target_employee_counts = ['1 to 10', '11 to 100', '101 to 1000', 'Small']
 
-    # target_actors = ['Guard', 'Doctor or nurse', 'Acquaintance', 'Call center', 'Customer', 'Manager', 'Cashier',
-    #                   'State-affiliated', 'End-user', 'Executive', 'System admin', 'Developer', 'Human resources',
-    #                   'Finance', 'Auditor', 'Former employee', 'Other', 'Competitor', 'Helpdesk', 'Unaffiliated',
-    #                   'Nation-state', 'Organized crime', 'Unknown', 'Maintenance', 'Activist', 'Terrorist',
-    #                   'Force majeure']
-    target_actors = ['Customer', 'State-affiliated', 'End-user', 'Former employee', 'Competitor', 'Nation-state',
-                     'Organized crime', 'Activist']
+    # target_actors = ['Guard', 'Doctor or nurse', 'Acquaintance', 'Call center', 'Customer',
+    #                  'Manager', 'Cashier', 'State-affiliated', 'End-user', 'Executive',
+    #                  'System admin', 'Developer', 'Human resources', 'Finance', 'Auditor',
+    #                  'Former employee', 'Other', 'Competitor', 'Helpdesk', 'Unaffiliated',
+    #                  'Nation-state', 'Organized crime', 'Unknown', 'Maintenance', 'Activist',
+    #                  'Terrorist', 'Force majeure']
+    target_actors = ['State-affiliated', 'Former employee', 'Competitor',
+                     'Nation-state', 'Organized crime', 'Activist']
 
     # All disclosure values: ['Yes', 'No', 'Potentially', 'Unknown']
 
-    # Saves actor names, overall fail rates, overall sample size, small business prevalence, and small business
-    # sample size to a provided file as a csv
+    # Saves actor names, overall fail rates, overall sample size, small business prevalence,
+    # and small business sample size to a provided file as a csv
     @classmethod
     def save_stats(cls, outfile_name):
         csv_rows = []
@@ -57,8 +59,8 @@ class Incident:
             csv_writer = csv.writer(outfile)
             csv_writer.writerows(csv_rows)
 
-    # Prints actor names, overall fail rates, overall sample size, small business prevalence, and small business
-    # sample size to stdout
+    # Prints actor names, overall fail rates, overall sample size, small business prevalence, and
+    # small business sample size to stdout
     @classmethod
     def print_stats(cls):
         cls.update_aggregate_statistics()
@@ -66,23 +68,29 @@ class Incident:
         actor_header = 'Actor'
         fail_rate_header = 'Fail Rate'
         n_header = 'n'
+        big_n_header = 'N'
         prevalence_rate_header = 'Prevalence Rate'
 
-        print(f'{actor_header:20s} {fail_rate_header:15s} {n_header:10s} {prevalence_rate_header:15s} {n_header:5s}')
-        print(f'{"-" * 70}')
+        print(f'{actor_header:20s} {fail_rate_header:15s} {n_header:10s} ', end='')
+        print(f'{big_n_header:10s} {prevalence_rate_header:17s} {n_header:5s} ', end='')
+        print(f'{big_n_header:5s}')
+        print(f'{"-" * 90}')
 
         for actor in cls.all_actor_fail:
             if (actor in cls.target_actors) and (actor in cls.sb_actor_prevalence):
                 fail_rate = f'{cls.all_actor_fail[actor]["fail-rate"]:0.3}'
                 fail_n = f'{cls.all_actor_fail[actor]["count"]}'
+                fail_big_n = f'{cls.all_actor_count}'
                 prevalence_rate = f'{cls.sb_actor_prevalence[actor]["prevalence-rate"]:0.3}'
                 prevalence_n = f'{cls.sb_actor_prevalence[actor]["count"]}'
+                prevalence_big_n = f'{cls.sb_incident_count}'
 
-                print(f'{actor:20s} {fail_rate:15s} {fail_n:10s} {prevalence_rate:15s} {prevalence_n:5s}')
+                print(f'{actor:20s} {fail_rate:15s} {fail_n:10s} {fail_big_n:10s}', end='')
+                print(f'{prevalence_rate:17s} {prevalence_n:5s} {prevalence_big_n}')
 
-    # Computes aggregate statistics about incidents using the class variables which track all instances of the Incident
-    # class. This includes fail rate and prevalence rate, and should be called after all instances of Incident have
-    # been created or before outputting statistics.
+    # Computes aggregate statistics about incidents using the class variables which track all
+    # instances of the Incident class. This includes fail rate and prevalence rate, and should be
+    # called after all instances of Incident have been created or before outputting statistics.
     @classmethod
     def update_aggregate_statistics(cls):
         # Compute fail rates based incidents with and without data breaches
@@ -107,13 +115,13 @@ class Incident:
 
     # Returns dictionary with elements of dictionary of dictionaries d sorted by d[i][p]
     @staticmethod
-    def sort_descending(d, p):
-        sorted_tuple_list = sorted(d.items(), key=lambda item: item[1][p])
+    def sort_descending(dictionary, sort_parameter):
+        sorted_tuple_list = sorted(dictionary.items(), key=lambda item: item[1][sort_parameter])
         sorted_tuple_list.reverse()
         return dict(sorted_tuple_list)
 
-    def __init__(self, ij):
-        self.incident_json = ij
+    def __init__(self, input_json):
+        self.incident_json = input_json
         self.employee_count = self.incident_json['victim']['employee_count']
 
         self.is_sb = self.get_is_sb()
@@ -122,7 +130,7 @@ class Incident:
 
         self.update_statistics()
 
-        # actor_to_investigate = 'Customer'
+        # actor_to_investigate = 'Competitor'
         # if actor_to_investigate in self.actor_varieties:
         #     print(f'{self.incident_json["summary"]}\n')
 
@@ -131,6 +139,7 @@ class Incident:
         # Update actor success for all breaches
         for actor_variety in self.actor_varieties:
             if self.is_breach is True:
+                Incident.all_actor_count = Incident.all_actor_count + 1
                 if actor_variety in Incident.all_actor_fail:
                     success_count = Incident.all_actor_fail[actor_variety]['success']
                     Incident.all_actor_fail[actor_variety]['success'] = success_count + 1
@@ -138,6 +147,7 @@ class Incident:
                     Incident.all_actor_fail[actor_variety] = {'success': 1, 'fail': 0, 'maybe': 0}
 
             elif self.is_breach is False:
+                Incident.all_actor_count = Incident.all_actor_count + 1
                 if actor_variety in Incident.all_actor_fail:
                     fail_count = Incident.all_actor_fail[actor_variety]['fail']
                     Incident.all_actor_fail[actor_variety]['fail'] = fail_count + 1
@@ -192,7 +202,8 @@ class Incident:
 
         return is_breach
 
-    # Returns a list of actor varieties involved in the incident. May contain the same variety multiple times.
+    # Returns a list of actor varieties involved in the incident. May contain the same variety
+    # multiple times.
     def get_actor_varieties(self):
         actor_categories = self.incident_json['actor'].values()
         actor_varieties = []
@@ -212,8 +223,8 @@ def main():
     # Create a new instance of Incident for each verified incident
     # Incidents are tracked in aggregate using class variables
     for file_path in file_paths:
-        with file_path.open() as f:
-            incident_json = json.load(f)
+        with file_path.open() as file:
+            incident_json = json.load(file)
             Incident(incident_json)
 
     # Print statistics to console and save them to a csv
